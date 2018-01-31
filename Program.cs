de09b7a4deff65609c491ff3e95e86d2de1b27b9
Jim Smyth <JamesSmyth@outlook.com>
2018-01-31 12:54:36 -0500

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("I am ");
+            MyInfo Jim = new MyInfo();
+            Jim.MyBirthDay = DateTime.Parse("01/29/1951");
+            Console.WriteLine(Jim.MyAge);
+        }
+    }
+
+    class MyInfo
+    {
+        public int MyAge
+        {
+            get 
+            { DateTime RightNowNoTime = DateTime.Now.Date;
+              int MyYears = RightNowNoTime.Year - MyBirthDay.Year;
+              return MyYears;
+      //        int months = RightNowNoTime.Month - MyBirthDay.Month;
+       //       int dayOfYearIWasBorn = MyBirthDay.DayOfYear;
+      //        int currentDayOfYear = RightNowNoTime.DayOfYear;
+      //        int DaysPastMyBirthDay = currentDayOfYear - dayOfYearIWasBorn;
+      //        bool haveIHadMyBirthday = (DaysPastMyBirthDay < 0);
+              
+
+            }
+        }
+
+        public DateTime MyBirthDay
+        {
+            get; set;
+        }
+
+
+    }
+}
